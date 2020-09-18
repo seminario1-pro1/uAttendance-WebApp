656c8d786e2cecf3e5e1cadca55418b933f47754
Guillermo Peitzner <guillermopeitzner@gmail.com>
2020-09-18 08:53:00 -0600

    [add] asistencias

--- src/app/components/assits/assits.component.ts
+++ src/app/components/assits/assits.component.ts
@@ -1,15 +1,71 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export interface StudentDetail {
+  student: string;
+  image: string;
+  present: number;
+  show: boolean;
+}
+
+export interface AssistsDetail {
+  assist: string;
+  image: string;
+  students: StudentDetail[];
+  show: boolean;
+}
 
 @Component({
   selector: 'app-assits',
   templateUrl: './assits.component.html',
-  styleUrls: ['./assits.component.css']
+  styleUrls: ['./assits.component.css'],
 })
 export class AssitsComponent implements OnInit {
+  bucketLink: string =
+    'https://pro1-images-grupo16.s3.us-east-2.amazonaws.com/';
+  assistsDetails: AssistsDetail[];
+  gettingData: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private httpClient: HttpClient) {
+    this.getAssists();
   }
 
+  getAssists(): void {
+    this.httpClient
+      .get(
+        'https://v3jx6tlas9.execute-api.us-east-2.amazonaws.com/prod/obtenerasistencias'
+      )
+      .subscribe(
+        (data: any[]) => {
+          this.assistsDetails = [];
+          data.forEach((assist) => {
+            let students: StudentDetail[] = [];
+            assist.students.forEach((studentDetail: any) => {
+              students.push({
+                student: this.cleanName(studentDetail.student),
+                image: this.bucketLink + studentDetail.student,
+                present: studentDetail.present,
+                show: false,
+              });
+            });
+            this.assistsDetails.push({
+              assist: this.cleanName(assist.assist),
+              image: this.bucketLink + assist.assist,
+              students: students,
+              show: false,
+            });
+          });
+          this.gettingData = false;
+        },
+        () => {}
+      );
+  }
+
+  cleanName(name: string): string {
+    let tmp: any[] = name.split('/');
+    let aux: any[] = tmp[1].split('.');
+    return aux[0].toString();
+  }
+
+  ngOnInit(): void {}
 }
